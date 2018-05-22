d4da515dce7a27358757a53fdbb7d07cf0d84061
rhe24 <rhe24@aber.ac.uk>
2018-05-23 00:40:28 +0100

    Added user generation script

---
+++ data/generate_user.php
@@ -0,0 +1,51 @@
+<?php
+/**
+  * Haulage Management System - User Generation Script for use by sysadmins
+  *
+  * @author Rhys Evans
+  * @version 22/05/2018
+  * 2018 (C) Rhys Evans
+*/
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+// Load .env file for sensitive configs
+$dotenv = new Dotenv\Dotenv(__DIR__ . '/../config/');
+$dotenv->load();
+
+// Setup database information from .env
+$dbservername = getenv("DB_HOST");
+$dbname = getenv("DB_DATABASE");
+$dbusername = getenv("DB_USERNAME");
+$dbpassword = getenv("DB_PASSWORD");
+
+// Connec to mysql database
+$connection = new mysqli($dbservername, $dbusername, $dbpassword, $dbname);
+
+if($connection->connect_error){
+  die("Connection to MySql failed: " . $connection->connect_error);
+}
+
+// Get user information from command line
+echo("Enter User Infromation Below to Register new User: \n");
+$username = readline('Username: ');
+$password = readline('Password: ');
+$name = readline('Name: ');
+$email = readline('Email: ');
+
+// Hash Password
+$password = password_hash($password, PASSWORD_DEFAULT);
+
+// Create User Record
+$sql = "INSERT INTO users (username, password, name, email) VALUES ('".$username."', '".$password."', '".$name."', '".$email."')";
+
+
+// Perform SQL creation query
+if($connection->query($sql) === TRUE){
+  echo "New User Registered Successfully\n";
+}else{
+  echo "Error: " . $sql . "\n" . $connection->error;
+}
+
+$connection->close();
+?>
